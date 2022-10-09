7b723922db4dbc00e6578ef18559f715d6497283
Miguel Pelegrina <migupele@gmail.com>
2022-10-09 10:47:39 +0200

    NO ME FUNCIONA BIEN LA LISTA, DEBE COMPROBAR LOS VALORES ANTERIRORES

---
+++ main.py
@@ -0,0 +1,53 @@
+lista = []
+entrada = ""
+esValido = False
+
+#def darDeBaja(codigo):
+    #for i in lista:
+
+#def buscar
+
+def contieneCodigo(codigo):
+    #NO ESTÁ BIEN TODAVÍA, ME DEBE A
+    for i in lista:
+        for clave, valor in i.items():
+            while valor == codigo:
+                codigo = input("Introduzca un código de artículo que NO esté ya en la lista")
+    return codigo
+
+def darDeAlta():
+    cod_articulo = input("Introduzca el código del producto que quiera dar de alta")
+    cod_articulo = contieneCodigo(cod_articulo)
+    nombre = input("Introduzca el nombre del producto")
+    descripcion = input("Introduzca la descripcion del producto")
+    precio = input("Introduzca el precio del producto")
+    diccionario = {"Código artículo": cod_articulo,
+                   "Nombre": nombre,
+                   "Descripcion": descripcion,
+                   "Precio": precio
+                   }
+    lista.append(diccionario)
+
+while entrada != "6":
+    entrada = input("Introduzca un numero de 1 a 6: " +
+        "\n 1: Para dar un producto de alta" +
+        "\n 2: Para dar un producto de baja" +
+        "\n 3: Para modificar un el nombre, la descripcion o el precio de un producto" +
+        "\n 4: Para buscar en función de una característica" +
+        "\n 5: Para listar todos los productos " +
+        "\n 6: Para salirse del programa")
+    if entrada == "1":
+        darDeAlta()
+    elif entrada == "2":
+        codigoArticulo = input("Introduzca el código del producto que quiera dar de baja")
+        #darDeBaja(codigoArticulo)
+    elif entrada == "3":
+        codigoArticulo = input("Introduzca el código del producto que quiera modificar")
+    elif entrada == "5":
+        for i in lista:
+            for clave, valor in i.items():
+                print(clave,valor)
+            print("\n")
+
+
+
