7155a5f4fa9369142856ff5ddbd3762cf4bd772c
Miguel Pelegrina <migupele@gmail.com>
2022-10-09 13:14:03 +0200

    YA DEBERÍA FUNCIOANR TODO

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ lista = []
 entrada = ""
 esValido = False
 
+
 def darDeBaja():
     codigo = input("Introduzca el código del producto que quiera dar de baja")
     existe = True
@@ -13,7 +14,8 @@ def darDeBaja():
         else:
             existe = False
     if(existe == False):
-        print("No se ha podido eliminar el producto porque no se encuentra en la lista")
+        print("El producto no se encuentra en la lista")
+
 
 def buscar():
     datoArticulo = input("Introduzca cualquier dato por el cúal quiera buscar un artículo")
@@ -23,13 +25,35 @@ def buscar():
                 for clave,valor in i.items():
                     print(clave,valor)
 
-#def modificar()
+
+def modificar():
+    codigo = input("Introduzca el código del producto que quiera modificar")
+    for i in lista:
+        if (i.get("Código artículo") == codigo):
+            modificacion = input("¿Qué característica del artículo desea modificar?"
+                  "\n 1: Para cambiar el nombre" +
+                  "\n 2: Para cambiar la descripcion" +
+                  "\n 3: Para cambiar el precio"
+                  )
+            if modificacion == "1":
+                valor = input("¿Cúal será el nuevo nombre?")
+                i.update({"Nombre":valor})
+            elif modificacion == "2":
+                valor = input("¿Cúal será la nueva descripción?")
+                i.update({"Descripción":valor})
+            elif modificacion == "3":
+                valor = input("¿Cúal será el nuevo precio?")
+                i.update({"Precio":valor})
+        else:
+            print("El producto no se encuentra en la lista")
+
 
 def contieneCodigo(codigo):
     for i in lista:
         if (i.get("Código artículo") == codigo):
             return True
     return False
+
 
 def darDeAlta():
     cod_articulo = input("Introduzca el código del producto que quiera dar de alta")
@@ -40,10 +64,11 @@ def darDeAlta():
     precio = input("Introduzca el precio del producto")
     diccionario = {"Código artículo": cod_articulo,
                    "Nombre": nombre,
-                   "Descripcion": descripcion,
+                   "Descripción": descripcion,
                    "Precio": precio
                    }
     lista.append(diccionario)
+
 
 while entrada != "6":
     entrada = input("Introduzca un numero de 1 a 6: " +
@@ -58,14 +83,10 @@ while entrada != "6":
     elif entrada == "2":
         darDeBaja()
     elif entrada == "3":
-        codigoArticulo = input("Introduzca el código del producto que quiera modificar")
+        modificar()
     elif entrada == "4":
         buscar()
     elif entrada == "5":
         for i in lista:
             for clave, valor in i.items():
                 print(clave,valor)
-            print("\n")
-
-
-
